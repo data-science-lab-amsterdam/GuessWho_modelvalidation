80337f5c66cb7332045defd5c7f3972fa098eccc
Demi den Daas <demi.dendaas@datasciencelab.nl>
2018-09-06 13:17:58 +0000

    Replace script.js

--- src/controlpage/assets/script.js
+++ src/controlpage/assets/script.js
@@ -1,24 +1,44 @@
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 
+var data;
 
 var read_data = function()
 {
     var raw_data = document.getElementById("data-container").getAttribute('accessKey');
-    var data = JSON.parse(decodeURIComponent(raw_data));
+    data = JSON.parse(decodeURIComponent(raw_data));
 
     // show image
     document.getElementById('image').src = data['url'];
 
     // show properties
     var features = data['features'];
-    for (var i = 0; i < features.length; i++) {
-        var item = features[i];
-        var feature = "hidden-" + item['key'];
-        console.log(feature);
+    //for (var i = 0; i < features.length; i++) {
+    for (var key in data['features']) {
+        var item = data['features'][key];
+        console.log(key);
         var value = item['value'];
-        console.log(value)
-        document.getElementById(feature).value = value;
+        var score = item['score'];
+        console.log(value);
+        //document.getElementById(feature).value = value;
+        selector = `#input-${feature} select option[value=${value}]`;
+        document.querySelector(selector).selected = true;
     }
+}
+
+var save_data = function() 
+{
+    // read values from dropdown fields
+    console.log(data['features'])
+    for (var i = 0; i < data['features'].length; i++) {
+        var selected_value;
+        opts = document.querySelectorAll(`#input-${feature} select option`);
+        opts.forEach(function(x) {
+            if (x.selected==true) {
+                selected_value = x.value;
+            }
+        });
+
+    // put json into hidden data container
 }
 
 var init = function() {
@@ -36,6 +56,8 @@ var init = function() {
         attributes: true,
         characterData: true
     });
+
+    document.getElementById('save-button').addEventListener('click', save_data);
 }
 
 
