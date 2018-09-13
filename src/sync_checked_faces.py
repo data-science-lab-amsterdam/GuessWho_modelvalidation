fed57ae2f37fa7a70a53e67dc13957e57a10bfb5
Robert van Straalen <robert.vanstraalen@datasciencelab.nl>
2018-09-13 13:26:56 +0200

    fixed sync script

--- src/sync_checked_faces.py
+++ src/sync_checked_faces.py
@@ -7,18 +7,25 @@ from pathlib import Path
 import os
 import subprocess
 import time
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 src_root = Path('/Volumes/data')
 tgt_root = Path('./data')
 
 images_path = Path('./images/faces_checked')
-labels_path = Path('./labels_chacked')
+labels_path = Path('./labels_checked')
 
 
 def get_most_recent(directory, look_for='*.jpg'):
     files = directory.glob(look_for)
-    return max(map(os.path.getctime, files))
+    try:
+        return max(map(os.path.getctime, files))
+    except:
+        return 0
 
 
 def get_files_since(directory, timestamp, look_for='*.jpg'):
@@ -28,32 +35,24 @@ def get_files_since(directory, timestamp, look_for='*.jpg'):
 
 def copy_files(src_files, tgt_dir):
     if len(src_files) == 0:
-        print("No new files, nothing to copy")
+        logging.info("No new files, nothing to copy")
         return 0
-    src_dir = str(src_files[0].parent)
-    filenames_no_path = [x.name for x in src_files]
-    cmd_parts = [
-        'cp'
-        src_dir + '/{' + ','.join(filenames_no_path) + '}',
-        str(tgt_dir)
-    ]
-    print("Executing command: ")
-    print(' '.join(cmd_parts))
-    res = subprocess.call(cmd_parts)
-    print('Result: {}'.format(res))
-    return res
+
+    for file in src_files:
+        logging.info(str(file))
+        res = subprocess.call(['cp', str(file), tgt_dir])
+        if res != 0:
+            logging.error('Something went wrong while copying!')
 
 
 def update(src, tgt, look_for):
-    most_recent_tgt = get_most_recent(dir=tgt,
+    most_recent_tgt = get_most_recent(directory=tgt,
                                       look_for=look_for
                                       )
-    new_files = get_files_since(dir=src,
+    new_files = get_files_since(directory=src,
                                 timestamp=most_recent_tgt,
                                 look_for=look_for
                                 )
-    print("Files to copy:")
-    print(new_files)
 
     return copy_files(new_files, tgt)
 
@@ -61,15 +60,15 @@ def update(src, tgt, look_for):
 if __name__ == '__main__':
     while True:
         if not src_root.exists():
-            print("Geen toegang tot {}".format(str(src_root)))
-            print("Check de verbinding!")
+            logging.info("Geen toegang tot {}".format(str(src_root)))
+            logging.info("Check de verbinding!")
 
         try:
             update(src_root/images_path, tgt_root/images_path, '*.jpg')
             update(src_root/labels_path, tgt_root/labels_path, '*.json')
         except Exception as e:
-            print(e)
-            print("Something went wrong. Will try again later")
+            logging.info(e)
+            logging.info("Something went wrong. Will try again later")
 
-        time.sleep(10)
+        time.sleep(5)
 
